3e7bdbe19693f34cfec83c2a7943aea9aa59331b
Tommy Westerhold <thomas.westerhold@vanderbilt.edu>
2020-08-04 23:10:40 -0600

    Normalized tables (#3)

--- MegatableTriggers.sql
+++ MegatableTriggers.sql
@@ -1,15 +1,18 @@
 USE NCAA;
 
-DROP TRIGGER IF EXISTS emptyMinutes;
+DROP TRIGGER IF EXISTS megatable_before_insert;
 
 DELIMITER //
 
-CREATE TRIGGER emptyMinutes
+CREATE TRIGGER megatable_before_insert
 BEFORE INSERT
 ON megatable
 FOR EACH ROW
 BEGIN
 	IF (NEW.minutes = '') THEN
 		SET NEW.minutes = '00:00';
+	END IF;
+    IF (NEW.tournament = '') THEN
+		SET NEW.tournament = NULL;
 	END IF;
 END //
